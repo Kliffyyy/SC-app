b56e59d677a62294aa793a02122d54ac9806e79c
Kliffyyy <88235200+Kliffyyy@users.noreply.github.com>
2023-03-21 14:56:37 +0800

    Initial Commit

---
+++ SC-app/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SC-app
+//
+//  Created by klifton Cheng stu on 21/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SC-app/SC_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  SC_appApp.swift
+//  SC-app
+//
+//  Created by klifton Cheng stu on 21/3/23.
+//
+
+import SwiftUI
+
+@main
+struct SC_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
