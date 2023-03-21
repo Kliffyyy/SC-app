8ca14383be8da8d6c353a7b4a80fa697ae446bc6
Kliffyyy <88235200+Kliffyyy@users.noreply.github.com>
2023-03-21 15:50:37 +0800

    toolbar popup added

--- SC-app/ContentView.swift
+++ SC-app/ContentView.swift
@@ -8,14 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var showingPopover = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        List{
+            
+        } .toolbar() {
+            ToolbarItem(placement: .primaryAction) {
+                Button{
+                    showingPopover = true
+                } label: {
+                    HStack{
+//                        Image(systemName: "macwindow.on.rectangle")
+                        Text("Window")
+                            .font(.headline)
+                            .padding(.trailing, 10)
+                        Image(systemName: "chevron.down")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 10)
+                            .offset(y: 1)
+                    }
+                }
+                .popover(isPresented: $showingPopover) {
+                    Text("Your content here")
+                        .font(.headline)
+                        .padding()
+                }
+            }
         }
-        .padding()
     }
 }
 
