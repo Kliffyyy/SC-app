78ef0f45f70eb6eb0ad2625c3f8d9683ff968bc1
Kliffyyy <88235200+Kliffyyy@users.noreply.github.com>
2023-04-03 19:32:27 +0800

    fixed the animations thanks to @cheesetan_ only bug now is that the icon can glitch out if spammed too fast. I have no clue if swiftUI supports finding out the sidebar state, because currently its just a @State variable holding my sidebar toggling together.

--- SC-app/ContentView.swift
+++ SC-app/ContentView.swift
@@ -19,6 +19,7 @@ struct ContentView: View {
     
     var body: some View {
         
+        
         NavigationView {
             SideBarView(subcomm: subcomm)
                 .frame(minWidth: 200, minHeight: 300)
@@ -26,22 +27,18 @@ struct ContentView: View {
                     ToolbarItem(placement: .primaryAction){
                         Button {
                             toggleSidebar()
-                            sidebarState.toggle()
-                            withAnimation(.easeInOut(duration: 2)) {
-                                sidebarX += sidebarState ? 14 : -14
-                            }
                         } label: {
 //                            Image(systemName: toolbarState ? "sidebar.left" : "sidebar.right")
 //                                .animation(.easeInOut(duration: 0.2), value: toolbarState)
                             
-                            CustomSideBarToggleIcon(controlPoint: controlPoint, toggle: sidebarState, x: sidebarX)
+                            CustomSideBarToggleIcon(controlPoint: controlPoint, x: sidebarX)
                                 .stroke(.primary, style: StrokeStyle(lineWidth: 1.7, lineCap: .round, lineJoin: .round))
                                 .frame(width: 20, height: 15)
                                 .offset(x: 0, y: 0)
                                 .scaledToFit()
                         }
 //                        .animation(.easeInOut(duration: 1), value: toolbarState)
-                        .animation(.linear(duration: 5), value: sidebarX)
+                        .animation(.easeInOut(duration: 0.2), value: sidebarX)
                     }
                     ToolbarItem(placement: .automatic) {
                         
@@ -76,38 +73,45 @@ struct ContentView: View {
     }
     
     private func toggleSidebar() {
-            NSApp.keyWindow?.firstResponder?.tryToPerform(#selector(NSSplitViewController.toggleSidebar(_:)), with: nil)
+        sidebarState.toggle()
+        sidebarX = sidebarState ? 17 : 3
+        NSApp.keyWindow?.firstResponder?.tryToPerform(#selector(NSSplitViewController.toggleSidebar(_:)), with: nil)
     }
 }
 
 struct CustomSideBarToggleIcon: Shape {
     var controlPoint: Double
-    var toggle: Bool
     var x: Double
+    var arcRadius: Double = 3
+    
+    var animatableData: Double {
+            get { x }
+            set { x = newValue }
+        }
     
     func path(in rect: CGRect) -> Path {
         var pencil = Path()
         
         // INSTRUCTIONS
         // 1. Move to an origin
-        pencil.move(to: CGPoint(x: rect.minX + 2, y: rect.minY))
-        pencil.addLine(to: CGPoint(x: rect.minX + 2, y: rect.minY))
+        pencil.move(to: CGPoint(x: rect.minX + arcRadius, y: rect.minY))
+        pencil.addLine(to: CGPoint(x: rect.minX + arcRadius, y: rect.minY))
         
-        pencil.addLine(to: CGPoint(x: rect.maxX - 2, y: rect.minY))
-        pencil.addArc(center: CGPoint(x: rect.maxX - 2, y: rect.minY + 2), radius: 2, startAngle: Angle(degrees: 270), endAngle: Angle(degrees: 0), clockwise: false)
-        pencil.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY - 2))
-        pencil.addArc(center: CGPoint(x: rect.maxX - 2, y: rect.maxY - 2), radius: 2, startAngle: Angle(degrees: 0), endAngle: Angle(degrees: 90), clockwise: false)
-        pencil.addLine(to: CGPoint(x: rect.minX + 2, y: rect.maxY))
-        pencil.addArc(center: CGPoint(x: rect.minX + 2, y: rect.maxY - 2), radius: 2, startAngle: Angle(degrees: 90), endAngle: Angle(degrees: 180), clockwise: false)
-        pencil.addLine(to: CGPoint(x: rect.minX, y: rect.minY + 2))
-        pencil.addArc(center: CGPoint(x: rect.minX + 2, y: rect.minY + 2), radius: 2, startAngle: Angle(degrees: 180), endAngle: Angle(degrees: 270), clockwise: false)
+        pencil.addLine(to: CGPoint(x: rect.maxX - arcRadius, y: rect.minY))
+        pencil.addArc(center: CGPoint(x: rect.maxX - arcRadius, y: rect.minY + arcRadius), radius: arcRadius, startAngle: Angle(degrees: 270), endAngle: Angle(degrees: 0), clockwise: false)
+        pencil.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY - arcRadius))
+        pencil.addArc(center: CGPoint(x: rect.maxX - arcRadius, y: rect.maxY - arcRadius), radius: arcRadius, startAngle: Angle(degrees: 0), endAngle: Angle(degrees: 90), clockwise: false)
+        pencil.addLine(to: CGPoint(x: rect.minX + arcRadius, y: rect.maxY))
+        pencil.addArc(center: CGPoint(x: rect.minX + arcRadius, y: rect.maxY - arcRadius), radius: arcRadius, startAngle: Angle(degrees: 90), endAngle: Angle(degrees: 180), clockwise: false)
+        pencil.addLine(to: CGPoint(x: rect.minX, y: rect.minY + arcRadius))
+        pencil.addArc(center: CGPoint(x: rect.minX + arcRadius, y: rect.minY + arcRadius), radius: arcRadius, startAngle: Angle(degrees: 180), endAngle: Angle(degrees: 270), clockwise: false)
 
         pencil.closeSubpath()
         
         // Draw Line
-        pencil.move(to: CGPoint(x: x, y: rect.minY + 4))
-        pencil.addLine(to: CGPoint(x: x, y: rect.minY + 4))
-        pencil.addLine(to: CGPoint(x: x, y: rect.maxY - 4))
+        pencil.move(to: CGPoint(x: x, y: rect.minY + 3.5))
+        pencil.addLine(to: CGPoint(x: x, y: rect.minY + 3.5))
+        pencil.addLine(to: CGPoint(x: x, y: rect.maxY - 3.5))
         pencil.closeSubpath()
         
         
